782fdcbbdf64a712c5444922ae6a978e591615f6
qugank <qiyongg@gmail.com>
2016-06-06 11:33:33 +0800

    add comments in initialopt

--- matlab/initialopt.m
+++ matlab/initialopt.m
@@ -1,11 +1,11 @@
 function opt = initialopt()
 %INITIAL initial the options and parameters 
 
-    % load the learned relative importance
+    % load the learned relative importance. You can specify it manually, the first dimension corresponding to proximity, and the second controls continuity.
     load(['./matlab/ranksvmwunion','.mat'],'w');
     opt.RelativeImp = w';
     
-    % the weight of label cost
+    % the weight of label cost, the larger C the fewer groups.
     opt.C = 0.5; 
     
 
